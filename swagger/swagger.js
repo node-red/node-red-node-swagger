87de56725522e08a6446ebe1cb4a8b4bd7dbf24e
sooann <tan_soo_ann@hotmail.com>
2021-04-09 16:47:15 +0800

    Testing Swagger variable

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -93,6 +93,7 @@ module.exports = function(RED) {
                     deprecated,
                     parameters: [...parameters, ...additionalParams],
                     responses,
+                    swagger,
                     swaggerDoc,
                     node
                 };
